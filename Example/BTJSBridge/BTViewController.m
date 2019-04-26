2fe46c6ad0444f9fb56ea6025d38762641daf869
wangjianyu <wangjianyu@weiboyi.com>
2019-04-26 16:12:10 +0800

    Initial commit

---
+++ Example/BTJSBridge/BTViewController.m
@@ -0,0 +1,29 @@
+//
+//  BTViewController.m
+//  BTJSBridge
+//
+//  Created by BrooksWon on 04/26/2019.
+//  Copyright (c) 2019 BrooksWon. All rights reserved.
+//
+
+#import "BTViewController.h"
+
+@interface BTViewController ()
+
+@end
+
+@implementation BTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
